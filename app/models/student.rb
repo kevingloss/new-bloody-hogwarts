c0ed9fbac7e42c356b96e3cc74c2f82cae678d45
Kevin Gloss <kgloss89@gmail.com>
2022-01-02 19:35:50 -0700

    Complete user story 1

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
+  has_many :student_courses
+  has_many :courses, through: :student_courses
+  
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :house, presence: true
 end
---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Students Index Page" do
+  before :each do
+    @harry = Student.create!(name: "Harry Potter", age: 14, house: "Gryffindor")
+    @draco = Student.create!(name: "Draco Malfoy", age: 13, house: "Slytherin")
+    @ron = Student.create!(name: "Ron Weasley", age: 15, house: "Gryffindor")
+    @luna = Student.create!(name: "Luna Lovegood", age: 12, house: "Ravenclaw")
+  end
+
+  it 'shows a list of students and their information' do
+    visit "/students"
+
+    within("#student-#{@harry.id}") do
+      expect(page).to have_content(@harry.name)
+      expect(page).to have_content(@harry.age)
+      expect(page).to have_content(@harry.house)
+    end
+
+    within("#student-#{@draco.id}") do
+      expect(page).to have_content(@draco.name)
+      expect(page).to have_content(@draco.age)
+      expect(page).to have_content(@draco.house)
+    end
+  end
+end
